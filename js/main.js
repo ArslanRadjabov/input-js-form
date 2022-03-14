2b0851f8d84e934cd520ed96f1f8299236cff795
ArslonRadjabov <arslonradjabovblog@gmail.com>
2022-03-14 09:10:47 +0500

    ss

---
+++ js/main.js
@@ -0,0 +1,32 @@
+const itemInput = document.querySelector('input[type="text"]');
+const form = document.querySelector("form");
+const list = document.querySelector("#lists");
+
+
+
+form.addEventListener("submit",addItem);
+
+function addItem(e){
+    e.preventDefault();
+
+
+    let newItem = itemInput.value;
+    let li = document.createElement("li");
+    li.className="list-group-item d-flex";
+    li.appendChild(document.createTextNode(newItem));
+    
+    
+    let exitBtn = document.createElement("bx");
+    exitBtn.className="bx bx-x bx-close";
+
+    li.appendChild(exitBtn)    
+    console.log(li);
+
+    list.appendChild(li);
+    itemInput.value="";
+
+
+    exitBtn.addEventListener("click", ()=> {
+        li.remove();
+    })
+}
