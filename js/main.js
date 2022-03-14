685fbf71fe9e0a71e8dc56946b63a555b2851615
ArslonRadjabov <arslonradjabovblog@gmail.com>
2022-03-14 12:14:14 +0500

    dd

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const itemInput = document.querySelector('input[type="text"]');
+const itemInput = document.querySelector('#adder');
 const form = document.querySelector("form");
 const list = document.querySelector("#lists");
 
@@ -14,7 +14,10 @@ function addItem(e){
     let li = document.createElement("li");
     li.className="list-group-item d-flex animate__bounceInRight";
     li.style ="animation-duration: 1s;"
-    li.appendChild(document.createTextNode(newItem));
+    // let elP = document.createElement("p");
+    li.innerHTML = `<p>${newItem}</p>`
+    // elP.appendChild(document.createTextNode(newItem));
+    // li.appendChild(elP);
     
     
     let exitBtn = document.createElement("bx");
@@ -30,4 +33,22 @@ function addItem(e){
     exitBtn.addEventListener("click", ()=> {
         li.remove();
     })
+}
+
+const search = document.querySelector("#search");
+
+search.addEventListener("keyup",filterItems);
+
+function filterItems(e) {
+    let text = e.target.value.toLowerCase();
+    let items = list.getElementsByTagName("li");
+
+    for (let i = 0; i < Array.from(items).length; i++) {
+        let itemName = items[i].textContent;
+        if (itemName.toLocaleLowerCase().indexOf(text) !=-1) {
+            items[i].className="list-group-item d-flex animate__bounceInRight";
+        }else{
+            items[i].className="list-group-item d-none animate__bounceInRight";
+        }
+    }
 }
